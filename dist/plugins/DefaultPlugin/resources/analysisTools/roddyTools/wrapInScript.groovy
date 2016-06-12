971ab73a6838d470150ddb45868ac5cc51e54c5a
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2016-06-12 12:17:07 +0200

    Advances for the wrapper script.
    - Turned it into a class
    - Started to make it testable with unit tests
    - Turned the script more to real Java code, with e.g. try/catch

--- dist/plugins/DefaultPlugin/resources/analysisTools/roddyTools/wrapInScript.groovy
+++ dist/plugins/DefaultPlugin/resources/analysisTools/roddyTools/wrapInScript.groovy
@@ -1,224 +1,278 @@
-#!/usr/bin/env /home/heinold/.roddy/runtimeDevel/groovy/bin/groovy
-import groovy.transform.Field
+@groovy.transform.CompileStatic
+public class CallWrapper {
 
-// Shebang line or groovy call??? Maybe just a synthetic script passed via pipe?
 
-// Some variables, taken from env and config
-@Field Map<String, String> cvalues
-@Field String lockCommand = "";
-@Field String unlockCommand = "";
-@Field String jobStateLogFile = "";
-@Field String jobStateLogsLockFile = "";
-@Field String RODDY_JOBID = "";
-@Field String RODDY_SCRATCH = "";
-@Field String TOOL_ID = "";
-@Field String WRAPPED_SCRIPT = "";
-@Field String LD_LIBRARY_PATH = "";
-@Field String JOB_PROFILER_BINARY = "";
-@Field String outputFileGroup = "";
-@Field boolean enableJobProfiling = false;
-@Field boolean runDockerized = false;
+    public static final String ABORTED = "ABORTED"
+    public static final String STARTED = "STARTED"
 
-// Load the config file
-Map<String, String> loadConfigurationValues() {
-    return new File(cvalues["CONFIG_FILE"]).readLines().collectEntries { String line -> def split = line.split("[=]"); [split[0], split.size() > 1 ? split[1..-1].join("=") : ""] }
-}
-
-// Load and correct all environment variables
-// Also load from parameter file, if applicable
-Map<String, String> getEnvironmentVariables() {
-    jobid = RODDY_JOBID.split("[.]")[0]
-    Map<String, String> cvalues = System.getenv()
-//    cvalues.findAll { String k, String v -> v.contains("RODDY_JOBID") || v.contains("-x") }.collectEntries {
-//        String k, String v ->
-//            [k, v]
-//    }
-    RODDY_PARENT_JOBS = cvalues["RODDY_PARENT_JOBS"] ?: ""
-    String parameterFile = cvalues["PARAMETER_FILE"] ?: ""
-    if (parameterFile)
-        cvalues += new File(parameterFile).readLines().collectEntries() { String line -> def split = line.replace("export ", "").split("[=]"); [split[0], split.size() > 1 ? split[1..-1].join("=") : ""] } + cvalues
-    return cvalues;
-}
-
-void checkAndSetJobVariables() {
-// This is much more complicated than it should be... Maybe because of a problem in IDEA
-    RODDY_JOBID = cvalues["RODDY_JOBID"] ?: ['/bin/bash', '-c', 'echo \$\$'].execute().text.replace("\n", "")
-
-    String defaultScratchDir = cvalues["defaultScratchDir"] ?: "/data/roddyScratch"
-    if (!cvalues["RODDY_SCRATCH"])
-        RODDY_SCRATCH = "${defaultScratchDir}/${RODDY_JOBID}"
-    File f = new File(RODDY_SCRATCH);
-}
-
-void fillVariables() {
-    cvalues = getEnvironmentVariables();
-    cvalues += loadConfigurationValues();
-    checkAndSetJobVariables();
-    TOOL_ID = cvalues["TOOL_ID"];
-    WRAPPED_SCRIPT = cvalues["WRAPPED_SCRIPT"];
-    JOB_PROFILER_BINARY = cvalues["JOB_PROFILER_BINARY"]
-    jobStateLogFile = cvalues["jobStateLogFile"];
-    enableJobProfiling = cvalues["enableJobProfiling"] ? Boolean.parseBoolean(cvalues["enableJobProfiling"]) : false;
-    outputFileGroup = cvalues["outputFileGroup"] ?: 'groups'.execute().text.split("[ ]")[0]
-}
-
-fillVariables()
-println("Read out variables and environment, now starting script.")
-// Check first, if we should run this script in a docker
-
-// Check if we need to change group and call again
-
-// Seems we can run the real stuff...
-
-// Define some methods first, they are mostly in their call order.
-int exec(String command, boolean full = true) {
-    //TODO What an windows systems?
-    def process = null;
-    if (full)
-        process = ["bash", "-c", command].execute();
-    else
-        process = command.execute();
-    final String separator = "\n"
-
-    process.waitForProcessOutput(System.out, System.out);
-    process.waitFor();
-    if (process.exitValue() > 0) {
-        def cmd = command;
-        def ev = process.exitValue()
-        println("Process could not be run" + separator + "\tCommand: sh -c " + cmd + separator + "\treturn code is: " + ev)
+    public static int main(String[] args) {
+        new CallWrapper().run();
     }
-    return process.exitValue();
-}
 
-boolean checkParentJobsAndAbortIfNecessary() {
-    //        # Check if the jobs parent jobs are stored and passed as a parameter. If so Roddy checks the job state logfile
-//        # if at least one of the parent jobs exited with a value different to 0.
-//        if [[ ! ${RODDY_PARENT_JOBS} = false ]]
-//        then
-//        # Now check all lines in the file
-//        strlen=`expr ${#RODDY_PARENT_JOBS} - 2`
-//        RODDY_PARENT_JOBS=${RODDY_PARENT_JOBS:1:strlen}
-//        for parentJob in ${RODDY_PARENT_JOBS[@]}; do
-//        [[ ${exitCode-} == 250 ]] && continue;
-//        result=`cat ${jobStateLogFile} | grep -a "^${parentJob}:" | tail -n 1 | cut -d ":" -f 2`
-//        [[ ! $result -eq 0 ]] && echo "At least one of this parents jobs exited with an error code. This job will not run." && startCode="ABORTED"
-//        done
-//        fi
+    /** Some variables, taken from env and config **/
+    Map<String, String> cvalues
 
-    //        [[ ${startCode} == 60000 || ${startCode} == "ABORTED" ]] && echo "Exitting because a former job died." && exit 250
-//        # Sleep a second before and after executing the wrapped script. Allow the system to get different timestamps.
-//                sleep 2
+    String lockCommand = "lockfile -s 1 -r 50"
 
-}
+    String unlockCommand = "rm -f"
 
-void checkWrapperScriptAndAbortIfNecessary() {
-//        # Check the wrapped script for existence
-//        [[ ${WRAPPED_SCRIPT-false} == false || ! -f ${WRAPPED_SCRIPT} ]] && startCode=ABORTED && echo "The wrapped script is not defined or not existing."
+    String jobStateLogFile = "";
 
-}
+    String jobStateLogsLockFile = "";
 
-void checkAndSetLockCommand() {
-    //         Check
-    jobStateLogsLockFile = "$jobStateLogFile~"
+    boolean RODDY_AUTOCLEANUP_SCRATCH = false;
 
-    //         Select the proper lock command. lockfile-create is not tested though.
-    lockCommand = "lockfile -s 1 -r 50"
-    unlockCommand = "rm -f"
+    String RODDY_JOBID = "";
 
-    useLockfile = true
-    if (!exec("if [[ -z `which lockfile` ]]; then echo A; fi")) useLockfile = false
-//        [[ ${useLockfile} == false ]] && lockCommand=lockfile-create && unlockCommand=lockfile-remove && echo "Set lockfile commands to lockfile-create and lockfile-remove"
-}
+    String RODDY_SCRATCH = "";
 
+    String RODDY_PARENT_JOBS = "";
 
-void resetLibraryPath() {
-    // Set LD_LIBRARY_PATH to LD_LIB_PATH, if the script was called recursively.
-    if (!cvalues["LD_LIB_PATH"]) LD_LIBRARY_PATH = cvalues["LD_LIB_PATH"];
-//    [[ ${debugWrapInScript-false} == true ]] && set -xv
-//    [[ ${debugWrapInScript-false} == false ]] && set +xv*/
-}
+    String TOOL_ID = "";
 
-void createJobDirectories() {
-    File roddyScratch = new File(RODDY_SCRATCH)
-    File temp = new File(cvalues["DIR_TEMP"])
-    if (!roddyScratch.exists()) roddyScratch.mkdir()
-    if (!temp.exists()) temp.mkdir();
-}
+    String WRAPPED_SCRIPT = "";
 
-void appendToJobStateLogFile(String code) {
-    // Rely on Linux / Bash to handle the file locks correctly.
-    if (!jobStateLogFile)
-        throw new RuntimeException("Could not add line to jobstate logfile, the variable is not set!")
-    String cmd =
-            """
-                ${lockCommand} ${jobStateLogsLockFile};
-                echo "${RODDY_JOBID}:${code}:"`date +"%s"`":${TOOL_ID}" >> ${jobStateLogFile};
-                ${unlockCommand} ${jobStateLogsLockFile};
-            """
-    println ""
-    exec(cmd);
+    String LD_LIBRARY_PATH = "";
 
-};
+    String JOB_PROFILER_BINARY = "";
 
-int runWrappedScript() {
-    // Create export variables.
+    String outputFileGroup = "";
 
-    // Call the tool, add variables, correct exports
-    //
-    String command = "";
-    if (LD_LIBRARY_PATH)
-        command += "export $LD_LIBRARY_PATH; "
-    command += "export $WRAPPED_SCRIPT; "
-    command += getEnvironmentVariables().collect { String k, String v -> "export $k=${v.replace("-x", "").replace('#', '$')}; " }.join()
-    command += "export outputFileGroup=$outputFileGroup; "
-    if (JOB_PROFILER_BINARY && enableJobProfiling)
-        command += JOB_PROFILER_BINARY
-    command += " bash -c " + cvalues["WRAPPED_SCRIPT"];
-    int exitCode = exec(command);
+    boolean enableJobProfiling = false;
 
-    println("Calling script ${WRAPPED_SCRIPT}");
+    boolean runDockerized = false;
 
-//        myGroup=`groups  | cut -d " " -f 1`
-//        outputFileGroup=${outputFileGroup-$myGroup}
+    boolean useLockfile
 
-    println("Exited script ${WRAPPED_SCRIPT} with value ${exitCode}")
-
-    Thread.sleep(2000);
-    return exitCode;
-}
-
-void performAutocleanup() {
-    //        # Set this in your command factory class, when roddy should clean up the dir for you.
-//        [[ ${RODDY_AUTOCLEANUP_SCRATCH-false} == "true" ]] && rm -rf ${RODDY_SCRATCH} && echo "Auto cleaned up RODDY_SCRATCH"
-
-}
-
-void finalChecksAndExit(int code) {
-    if (code == 0) System.exit(0);
-    if (code == 100) {
-        println "Finished script with 99 for compatibility reasons with Sun Grid Engine. 100 is reserved for SGE usage."
-        System.exit 99
+    // Load the config file
+    Map<String, String> loadConfigurationValues() {
+        return new File(cvalues["CONFIG_FILE"]).readLines().collectEntries { String line -> def split = line.split("[=]"); [split[0], split.size() > 1 ? split[1..-1].join("=") : ""] }
     }
-    System.exit(code);
+
+    // Load and correct all environment variables
+    // Also load from parameter file, if applicable
+    Map<String, String> getEnvironmentVariables() {
+        Map<String, String> cvalues = System.getenv()
+        RODDY_PARENT_JOBS = cvalues["RODDY_PARENT_JOBS"] ?: ""
+        String parameterFile = cvalues["PARAMETER_FILE"] ?: ""
+        if (parameterFile)
+            cvalues += new File(parameterFile).readLines().collectEntries() { String line -> def split = line.replace("export ", "").split("[=]"); [split[0], split.size() > 1 ? split[1..-1].join("=") : ""] } + cvalues
+        return cvalues;
+    }
+
+    void checkAndSetJobVariables() {
+        // This is much more complicated than it should be... Maybe because of a problem in IDEA
+        RODDY_JOBID = cvalues["RODDY_JOBID"] ?: ['/bin/bash', '-c', 'echo \$\$'].execute().text.replace("\n", "")
+
+        String defaultScratchDir = cvalues["defaultScratchDir"] ?: "/data/roddyScratch"
+        if (!cvalues["RODDY_SCRATCH"])
+            RODDY_SCRATCH = "${defaultScratchDir}/${RODDY_JOBID}"
+        File f = new File(RODDY_SCRATCH);
+    }
+
+    void fillVariables() {
+        def errors = []
+        cvalues = getEnvironmentVariables();
+        cvalues += loadConfigurationValues();
+        checkAndSetJobVariables();
+
+        TOOL_ID = cvalues["TOOL_ID"];
+        WRAPPED_SCRIPT = cvalues["WRAPPED_SCRIPT"];
+        enableJobProfiling = cvalues["enableJobProfiling"] ? Boolean.parseBoolean(cvalues["enableJobProfiling"]) : false;
+        JOB_PROFILER_BINARY = cvalues["JOB_PROFILER_BINARY"]
+        jobStateLogFile = cvalues["jobStateLogFile"];
+        outputFileGroup = cvalues["outputFileGroup"] ?: 'groups'.execute().text.split("[ ]")[0]
+
+        if (!RODDY_JOBID) errors << "The job id for this job is not available, RODDY_JOBID is not set and could not be determined."
+        if (!RODDY_SCRATCH) errors << "The scratch dir is not available, RODDY_SCRATCH is not set and could not be determined."
+        if (!TOOL_ID) errors << "The variable 'TOOL_ID' is not set."
+        if (!WRAPPED_SCRIPT) errors << "The variable 'WRAPPED_SCRIPT' is not set."
+        if (!jobStateLogFile) errors << "The variable 'jobStateLogFile' is not set"
+        if (enableJobProfiling && !JOB_PROFILER_BINARY) errors << "Job profiling is requested but JOB_PROFILER_BINARY is not set."
+
+        if (errors) {
+            throw new RuntimeException("Some necessary variables are not set, aborting:\n" + errors.join("\n"));
+        }
+
+        println("Read out variables and environment, script can start now.")
+    }
+
+    // Define some methods first, they are mostly in their call order.
+    int exec(String command, boolean full = true) {
+        //TODO What an windows systems?
+        def process = null;
+        if (full)
+            process = ["bash", "-c", command].execute();
+        else
+            process = command.execute();
+        final String separator = "\n"
+
+        process.waitForProcessOutput(System.out, System.out);
+        process.waitFor();
+        if (process.exitValue() > 0) {
+            def cmd = command;
+            def ev = process.exitValue()
+            println("Process could not be run" + separator + "\tCommand: sh -c " + cmd + separator + "\treturn code is: " + ev)
+        }
+        return process.exitValue();
+    }
+
+    boolean checkParentJobsAndAbortIfNecessary() {
+        if (RODDY_PARENT_JOBS) {
+            String jobids = RODDY_PARENT_JOBS.replace("(", "").replace(")", "").trim() // Remove prefix and suffix, if applicable
+            jobids.split("[ ]").each {
+                String id ->
+                    // The 250 check was in the original wrapper script. I don't know why, I don't know how, keep it as a reminder?
+                    //        [[ ${exitCode-} == 250 ]] && continue;
+                    String status = new File(jobStateLogFile).readLines().findAll { String line -> line.startsWith(id) }?.last()?.split("[:]")[1] ?: "0"
+                    if (status.toInteger() > 0) {
+                        println "At least one of this parents jobs exited with an error code. This job will not run."
+                    }
+            }
+        }
+    }
+
+    void checkAndSetLockCommand() {
+        //         Check
+        jobStateLogsLockFile = "$jobStateLogFile~"
+
+        //         Select the proper lock command. lockfile-create is not tested though.
+        useLockfile = true
+        if (!exec("if [[ -z `which lockfile` ]]; then echo A; fi")) useLockfile = false
+
+        if (!useLockfile) {
+            lockCommand = "lockfile-create"
+            unlockCommand = "lockfile-remove"
+            println("Set lockfile commands to lockfile-create and lockfile-remove")
+        }
+    }
+
+    /** Set LD_LIBRARY_PATH to LD_LIB_PATH, if the script was called recursively. **/
+    void resetLibraryPath() {
+        if (!cvalues["LD_LIB_PATH"]) LD_LIBRARY_PATH = cvalues["LD_LIB_PATH"];
+    }
+
+    boolean createJobDirectories() {
+        def errors = [];
+
+        File roddyScratch = new File(RODDY_SCRATCH)
+        File temp = new File(cvalues["DIR_TEMP"])
+        if (!roddyScratch.exists()) roddyScratch.mkdir()
+        if (!temp.exists()) temp.mkdir();
+
+        if (!roddyScratch.exists()) errors << "Could not create directory: ${roddyScratch}";
+        if (!temp.exists()) errors << "Could not create temporary directory: ${temp}";
+
+        if (errors) {
+            println(errors.join("\n"))
+            throw new IOException(errors.join("\n"));
+        }
+    }
+
+    String createExportsForEnvironmentVariables() {
+        getEnvironmentVariables().collect { String k, String v -> "export $k=${v.replace("-x", "").replace('#', '$')}; " }.join("")
+    }
+
+    void appendToJobStateLogFile(String code) {
+        // Rely on Linux / Bash to handle the file locks correctly.
+        if (!jobStateLogFile)
+            throw new RuntimeException("Could not add line to jobstate logfile, the variable is not set!")
+        String cmd =
+                """
+                    ${lockCommand} ${jobStateLogsLockFile};
+                    echo "${RODDY_JOBID}:${code}:"`date +"%s"`":${TOOL_ID}" >> ${jobStateLogFile};
+                    ${unlockCommand} ${jobStateLogsLockFile};
+                """
+        println ""
+        exec(cmd);
+
+    };
+
+    int runWrappedScript() {
+        println("Calling script ${WRAPPED_SCRIPT}");
+
+        // Call the tool, add variables, correct exports
+        String command = "";
+
+        // Create exports for call
+        if (LD_LIBRARY_PATH)
+            command += "export $LD_LIBRARY_PATH; "
+        command += "export $WRAPPED_SCRIPT; "
+        command += createExportsForEnvironmentVariables()
+        command += "export outputFileGroup=$outputFileGroup; "
+
+        // Put in profiler
+        if (JOB_PROFILER_BINARY && enableJobProfiling)
+            command += JOB_PROFILER_BINARY
+
+        // Append the called command
+        command += " bash -c " + cvalues["WRAPPED_SCRIPT"];
+
+        // And execute...
+        int exitCode = exec(command);
+
+        println("Exited script ${WRAPPED_SCRIPT} with value ${exitCode}")
+
+        Thread.sleep(2000);
+        return exitCode;
+    }
+
+    void performAutocleanup() {
+        // Set this in your command factory class, when roddy should clean up the dir for you.
+        if (RODDY_AUTOCLEANUP_SCRATCH) (new File(RODDY_SCRATCH)).deleteDir();
+    }
+
+    int performFinalChecks(int code) {
+        if (code == 0) System.exit(0);
+        if (code == 100) {
+            println "Wrapped script '${WRAPPED_SCRIPT}' exited with code 100. Finishing script with 99 for compatibility reasons with Sun Grid Engine. 100 is reserved for SGE usage."
+            return 99;
+        }
+        return code;
+    }
+
+    CallWrapper() {}
+
+    int run() {
+
+        try {
+            fillVariables()
+
+            // Check first, if we should run this script in a docker
+
+            // Check if we need to change group and call again
+
+            // Seems we can run the real stuff...
+
+            // Perform initial tests
+            checkParentJobsAndAbortIfNecessary()
+
+            // Setup environment
+            checkAndSetLockCommand()
+            resetLibraryPath()
+            checkAndSetJobVariables()
+
+            // Create directories
+            createJobDirectories()
+        } finally {
+            appendToJobStateLogFile(ABORTED);
+        }
+
+        int exitcode = 240;
+
+        try {
+            // Run the job
+            appendToJobStateLogFile(STARTED);
+
+            exitcode = performFinalChecks(runWrappedScript());
+
+            // Finalize
+            performAutocleanup()
+        } finally {
+            appendToJobStateLogFile("" + exitcode);
+        }
+
+        return exitcode;
+    }
+
 }
-
-// Perform initial tests
-checkWrapperScriptAndAbortIfNecessary()
-checkParentJobsAndAbortIfNecessary()
-
-// Setup environment
-checkAndSetLockCommand()
-resetLibraryPath()
-checkAndSetJobVariables()
-
-// Run the job
-appendToJobStateLogFile("STARTED");
-
-createJobDirectories()
-int exitcode = runWrappedScript();
-
-appendToJobStateLogFile("" + exitcode);
-
-// Finalize
-performAutocleanup()
-finalChecksAndExit(exitcode)
---
+++ dist/plugins/DefaultPlugin/src/test/groovy/roddyTools/CallWrapperTest.java
@@ -0,0 +1,91 @@
+package roddyTools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by heinold on 09.06.16.
+ */
+public class CallWrapperTest {
+    @Test
+    public void loadConfigurationValues() throws Exception {
+
+    }
+
+    @Test
+    public void getEnvironmentVariables() throws Exception {
+
+    }
+
+    @Test
+    public void checkAndSetJobVariables() throws Exception {
+
+    }
+
+    @Test
+    public void fillVariables() throws Exception {
+
+    }
+
+    @Test
+    public void exec() throws Exception {
+
+    }
+
+    @Test
+    public void exec1() throws Exception {
+
+    }
+
+    @Test
+    public void checkParentJobsAndAbortIfNecessary() throws Exception {
+
+    }
+
+    @Test
+    public void checkAndSetLockCommand() throws Exception {
+
+    }
+
+    @Test
+    public void resetLibraryPath() throws Exception {
+
+    }
+
+    @Test
+    public void createJobDirectories() throws Exception {
+
+    }
+
+    @Test
+    public void createExportsForEnvironmentVariables() throws Exception {
+
+    }
+
+    @Test
+    public void appendToJobStateLogFile() throws Exception {
+
+    }
+
+    @Test
+    public void runWrappedScript() throws Exception {
+
+    }
+
+    @Test
+    public void performAutocleanup() throws Exception {
+
+    }
+
+    @Test
+    public void performFinalChecks() throws Exception {
+
+    }
+
+    @Test
+    public void run() throws Exception {
+
+    }
+
+}
